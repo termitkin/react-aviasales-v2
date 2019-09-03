077f85a000911cb1f7476cf34c9cfce723ca8f0a
termitkin <bigos911@gmail.com>
2019-09-03 15:35:55 +0300

    build folder removed from gitignore

---
+++ build/precache-manifest.e85a5950b606732ea75f1254220b9d3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f8b44344e63a8736a0b61a0d2fa91d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d8bc12770fcd3dfdae1",
+    "url": "/static/css/main.f3c945ab.chunk.css"
+  },
+  {
+    "revision": "96992008726fbcb03164",
+    "url": "/static/js/2.776a0e52.chunk.js"
+  },
+  {
+    "revision": "7d8bc12770fcd3dfdae1",
+    "url": "/static/js/main.1ae88aac.chunk.js"
+  },
+  {
+    "revision": "cdb2f5364a2c4e67e99c",
+    "url": "/static/js/runtime~main.d79c3d68.js"
+  },
+  {
+    "revision": "b8070e66e3dcab6db60bd9d5005de3e2",
+    "url": "/static/media/check.b8070e66.svg"
+  },
+  {
+    "revision": "eb515c4112576007ef53b00094e8921c",
+    "url": "/static/media/logo.eb515c41.svg"
+  }
+]);
