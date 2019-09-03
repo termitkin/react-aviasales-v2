e4e57bcb5cf9b72c4f415fb4a2211a21a42893a8
termitkin <bigos911@gmail.com>
2019-09-03 18:26:34 +0300

    rebuild

---
+++ build/precache-manifest.6d84ff9584c2dc62544141d0238e5717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407d825e2845b2827f43a5e0ef35d8b1",
+    "url": "/react-aviasales-v2/index.html"
+  },
+  {
+    "revision": "2da50f82b33b0face8b9",
+    "url": "/react-aviasales-v2/static/css/main.f3c945ab.chunk.css"
+  },
+  {
+    "revision": "a8ce9d21a7ffe0e5c343",
+    "url": "/react-aviasales-v2/static/js/2.9fdcb818.chunk.js"
+  },
+  {
+    "revision": "2da50f82b33b0face8b9",
+    "url": "/react-aviasales-v2/static/js/main.77710bd3.chunk.js"
+  },
+  {
+    "revision": "bee5aea8ebb2adfda167",
+    "url": "/react-aviasales-v2/static/js/runtime~main.1c411699.js"
+  },
+  {
+    "revision": "b8070e66e3dcab6db60bd9d5005de3e2",
+    "url": "/react-aviasales-v2/static/media/check.b8070e66.svg"
+  },
+  {
+    "revision": "eb515c4112576007ef53b00094e8921c",
+    "url": "/react-aviasales-v2/static/media/logo.eb515c41.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e85a5950b606732ea75f1254220b9d3e.js"
+  "/react-aviasales-v2/precache-manifest.6d84ff9584c2dc62544141d0238e5717.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-aviasales-v2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
