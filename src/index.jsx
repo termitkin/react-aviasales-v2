3efaf9a107bc2722f3fc8c5b404322e0500d0ab1
termitkin <bigos911@gmail.com>
2019-09-09 16:27:20 +0300

    removed polyfills for ie

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,3 @@
-import "react-app-polyfill/ie11";
-import "core-js/es/map";
-import "core-js/es/set";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
