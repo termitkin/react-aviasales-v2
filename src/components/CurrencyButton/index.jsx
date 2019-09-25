71f0d6192ecf8f31699a72c602b37669fbc69385
termitkin <bigos911@gmail.com>
2019-09-25 19:57:21 +0300

    nth-child replaced with nth-of-type

--- src/components/CurrencyButton/index.jsx
+++ src/components/CurrencyButton/index.jsx
@@ -23,8 +23,8 @@ const CurrencyButton = props => {
     transition: color, background-color, border 0.15s;
     border-radius: ${props.borderRadius || "0 0 0 0"};
 
-    &:nth-child(2),
-    &:nth-child(3) {
+    &:nth-of-type(2),
+    &:nth-of-type(3) {
       margin-left: -1px;
     }
 
