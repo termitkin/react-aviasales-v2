b3e7b50b9502b0ffa9ee19226d39d57457b8cb51
termitkin <bigos911@gmail.com>
2020-01-10 15:50:16 +0000

    Deploying to gh-pages from master 564d699801aaed9f346138886f070922226d3ad1

---
+++ precache-manifest.c85711714b7da4c283bb9e33e5abf4d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28aa763e14b4dc360af642d45e3d4db",
+    "url": "/react-aviasales-v2/index.html"
+  },
+  {
+    "revision": "6eb64b58830bc5344fdb",
+    "url": "/react-aviasales-v2/static/css/main.95cc706b.chunk.css"
+  },
+  {
+    "revision": "7f742376cb03982e55da",
+    "url": "/react-aviasales-v2/static/js/2.142e934e.chunk.js"
+  },
+  {
+    "revision": "6eb64b58830bc5344fdb",
+    "url": "/react-aviasales-v2/static/js/main.d94376f3.chunk.js"
+  },
+  {
+    "revision": "1bdf8e95bced45922c4e",
+    "url": "/react-aviasales-v2/static/js/runtime~main.9d2bdbe9.js"
+  },
+  {
+    "revision": "8490a9b4ee19fadab235e947d0e935e5",
+    "url": "/react-aviasales-v2/static/media/check.8490a9b4.svg"
+  },
+  {
+    "revision": "18ef78fbbd666af091cf1afc400e8268",
+    "url": "/react-aviasales-v2/static/media/logo.18ef78fb.svg"
+  }
+]);
